a66abab681b04694c3746d2207256d6400968a40
RcrossF <16991582+RcrossF@users.noreply.github.com>
2022-07-25 13:52:59 -0700

    Fix some problems with commitment checking always being invalid

--- auction.sol
+++ auction.sol
@@ -4,11 +4,9 @@ Implements a sealed bid auction where bidders know who they are bidding against 
 The creator of the contract is automatically set to be the auctioneer
 Users can start bidding after the contract is constructed. This phase is active until revealBlock.
 Next the reveal phase begins where willing users reveal their bid amount by calling reveal(). This phase is active until endBlock. The user must submit their bid amount in ETH alongside the reveal data to be considered in the auction
-
 Next, both of these can happen:
 1) Losing bidders can withdraw their bids only once they reveal
 2) The auctioneer can withdraw the highest revealed bid (the winner)
-
 The winning bidder's address is also logged after endBlock
 */
 contract SealedBidAuction {
@@ -29,37 +27,37 @@ contract SealedBidAuction {
 
    // Modifiers
    modifier onlyAuctioneer {
-      require(msg.sender == auctioneer);
+      require(msg.sender == auctioneer, "Only the auctioneer can do this");
       _;
    }
 
    modifier notAuctioneer {
-      require(msg.sender != auctioneer);
+      require(msg.sender != auctioneer, "This auctioneer cannot do this");
       _;
    }
 
    modifier biddingValid {
-      require(startBlock <= block.number && block.number < endBlock);
+      require(startBlock <= block.number && block.number < endBlock, "Bids must be within block range");
       _;
    }
 
    modifier afterEnd {
-      require(block.number >= endBlock);
+      require(block.number >= endBlock, "This can only happen after the end block");
       _;
    }
 
    modifier notCancelled {
-      require(!cancelled);
+      require(!cancelled, "The auction must not be cancelled");
       _;
    }
 
    modifier cancelledOrEnded {
-      require(cancelled || block.number >= endBlock);
+      require(cancelled || block.number >= endBlock, "The auction must not be cancelled or ended");
       _;
    }
 
    modifier notWinner {
-       require(bids[msg.sender] != winningBid);
+       require(bids[msg.sender] != winningBid, "The winner cannot do this");
        _;
    }
 
@@ -69,7 +67,7 @@ contract SealedBidAuction {
    }
 
    modifier revealPhase {
-      require(block.number < endBlock && block.number >= revealBlock, "Not in reveal phase");
+      require(block.number < endBlock && block.number >= revealBlock, "Not in reveal phase.");
       _;
    }
 
@@ -87,13 +85,12 @@ contract SealedBidAuction {
       endBlock = endBlock_;
    }
 
-
    function bid(bytes32 commit_hash) public notCancelled biddingValid notAuctioneer bidOnce {
       commitments[msg.sender] = commit_hash;
    }
 
-   function reveal(uint256 bid_val, uint256 nonce) public payable notCancelled notAuctioneer revealPhase {
-      bytes32 compare = encode(bid_val, msg.sender, nonce);
+   function reveal(uint256 bid_val, uint32 nonce) public payable notCancelled notAuctioneer revealPhase {
+      bytes32 compare = encode(bid_val, nonce);
       require(commitments[msg.sender] == compare, "Invalid commitment");
       require(msg.value == bid_val, "Payment does not match commited bid");
       if(msg.value > winningBid) {
@@ -103,8 +100,9 @@ contract SealedBidAuction {
    }
 
    // Encode given data to verify a user indeed made a commitment
-   function encode(uint256 val, address addr, uint256 nonce) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked(nonce, val, addr));
+   function encode(uint256 val, uint256 nonce) public pure returns (bytes32) {
+        bytes memory encoded = abi.encodePacked(val, nonce);
+        return keccak256(encoded);
     }
 
    function withdrawWinningBid() public payable notCancelled afterEnd onlyAuctioneer {
