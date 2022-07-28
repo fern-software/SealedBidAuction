a266bf9018b4ea85f43b1743526638cbd3255e41
Owen Jaques <owenjaques2000@gmail.com>
2022-07-28 02:48:37 -0700

    Typo.

--- frontend/src/components/RevealPhase.js
+++ frontend/src/components/RevealPhase.js
@@ -5,7 +5,7 @@ const RevealPhase = ({formBid, revealBid, phase}) => {
 	return (
 		<div>
 			<Alert variant="dark">
-				You bid {formBid} Wei. <br />
+				You bet {formBid} Wei. <br />
 				{phase !== Phases.reveal && "Please wait until the reveal phase to see if you bet enough 💃"}
 			</Alert>
 			{phase === Phases.reveal && <Button variant="dark" onClick={revealBid}>Reveal</Button>}
